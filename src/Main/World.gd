9f32e66ba2b06bcfded36c2e38dab004c579524f
Zhdanov Roman <zhdanov.roma@ulh.ru>
2022-12-12 16:19:14 +0300

    Added error handling on loading GameOver scene when player dies.

--- src/Main/World.gd
+++ src/Main/World.gd
@@ -3,4 +3,8 @@ extends Node2D
 
 
 func _on_Player_died():
-	get_tree().change_scene("res://src/Screens/GameOver.tscn") # Replace with function body.
+	var res = get_tree().change_scene("res://src/Screens/GameOver.tscn")
+	if (res == ERR_CANT_OPEN):
+		printerr("Can't open the GameOver scene, the path is wrong!")
+	if (res == ERR_CANT_CREATE):
+		printerr("The GameOver scene cannot be instantiated!")
