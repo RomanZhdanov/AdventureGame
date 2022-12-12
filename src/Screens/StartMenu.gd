d8ec8672805885bfb90ebb43b5b39cae07ea26a9
Zhdanov Roman <zhdanov.roma@ulh.ru>
2022-12-12 16:15:30 +0300

    Added error handling on changing menu to World scene.

--- src/Screens/StartMenu.gd
+++ src/Screens/StartMenu.gd
@@ -1,7 +1,11 @@
 extends Control
 
 func _on_StartButton_pressed():
-	get_tree().change_scene("res://src/Main/World.tscn")
+	var res = get_tree().change_scene("res://src/Main/World.tscn")
+	if (res == ERR_CANT_OPEN):
+		printerr("Can't open the World scene, the path is wrong!")
+	if (res == ERR_CANT_CREATE):
+		printerr("The World scene cannot be instantiated!")
 
 
 func _on_QuitButton_pressed():
