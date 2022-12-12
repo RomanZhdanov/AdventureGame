53251525607dd2ab8e1276ef549f98524f55f570
Zhdanov Roman <zhdanov.roma@ulh.ru>
2022-12-12 18:13:28 +0300

    Fixed error with add_child() method when creating new Berries.

--- src/World/Grass.gd
+++ src/World/Grass.gd
@@ -10,7 +10,7 @@ func create_grass_effect():
 
 func create_berry():
 	var berry = Berry.instance()
-	get_parent().add_child(berry)
+	get_parent().call_deferred("add_child", berry)
 	berry.global_position = global_position
 
 func _on_Hurtbox_area_entered(_area):
