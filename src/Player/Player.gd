1063413105a654007eaf16d3ae01fccc5bb3c2ba
Zhdanov Roman <zhdanov.roma@ulh.ru>
2022-12-26 22:16:07 +0300

    Fixed warning about unused event variable in _unhandled_input method.

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -57,7 +57,7 @@ func move_state(delta):
 	if Input.is_action_just_pressed("roll"):
 		state = ROLL
 
-func _unhandled_input(event):
+func _unhandled_input(_event):
 	if Input.is_action_just_pressed("use"):
 		interactionManager.initiate_interaction()
 
